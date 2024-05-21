721dd6f565a0d987f5813d6d93f84b433e6cc998
Adarsh <adarshgupta7103@gmail.com>
2024-05-21 07:26:56 +0530

    Adding items to the list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Items from "./Items";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const inputChangeHandler = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addingItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, input];
+    });
+    setInput("");
+  };
+
   return (
     <>
       <div className="full-page">
         <div className="main">
           <h2 className="heading">ToDo List</h2>
-          <input className="input" type="text" placeholder="Add a task" />
-          <button className="btn">+</button>
+          <input
+            className="input"
+            type="text"
+            placeholder="Add a task"
+            value={input}
+            onChange={inputChangeHandler}
+          />
+          <button onClick={addingItems} className="btn">
+            +
+          </button>
           <ol>
-            <li>Buy Mango</li>
+            {items.map((itemVal) => {
+              return <Items data={itemVal} />;
+            })}
           </ol>
         </div>
       </div>
---
+++ src/Items.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Items = (props) => {
+  return (
+    <div>
+      <li>{props.data}</li>
+    </div>
+  );
+};
+
+export default Items;
